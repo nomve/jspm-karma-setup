cf0b8ae0b049ec27607306fdd72cef4f3e022efd
Goran Košutić <gkosutic@tofex.de>
2016-08-19 15:23:08 +0200

    add sinon to setup

--- tests/test.js
+++ tests/test.js
@@ -4,4 +4,8 @@ describe('test setup', function() {
     it('should succesfully run', function() {
         expect(replacemodulename()).to.equal(true);
     });
+    it('should include sinon in tests', function() {
+        let spy = sinon.spy();
+        expect(spy.callCount).to.equal(0);
+    });
 });
